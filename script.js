fd9ab2919ab8bbbb91f8083187ff0e58fb494b61
Heather Smith <halexsmith86@gmail.com>
2021-09-21 13:13:47 -0400

    styled button hover state, added check functions

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+ // fires the functions when the button is clicked
+function submitForm(){
+    checkFirstName();
+    checkLastName();
+    checkEmail();
+    checkPassword();
+}
 
-//function that checks if user entry is a valid email address
+// checks if the user entered a first name
+function checkFirstName(firstName) {
+    var firstName = $('input[name=first]').val();
+    if(firstName == false){
+        var firstError = document.querySelector('#firsterror');
+        var ex1 = document.querySelector('#ex1');
+        var firstInput = document.querySelector('#input1')
+        // unhide the error message
+        firstError.style.visibility = "visible";
+        // unhide the exclamation point
+        ex1.style.display = "block";
+        // turn the border around the input field red
+        firstInput.style.border = "2px solid #FF7979";
+    }
+}
+
+// checks if the user entered a last name
+function checkLastName(lastName) {
+    var lastName = $('input[name=last]').val();
+    if(lastName == false){
+        var lastError = document.querySelector('#lasterror');
+        var ex2 = document.querySelector('#ex2');
+        var lastInput = document.querySelector('#input2')
+        // unhide the error message
+        lastError.style.visibility = "visible";
+        // unhide the exclamation point
+        ex2.style.display = "block";
+        // turn the border around the input field red
+        lastInput.style.border = "2px solid #FF7979";
+    }
+}
+
+// checks if user entry is a valid email address
 function checkEmail(email) {
     // selects the email input field's value
     var email = $('input[name=email]').val();
@@ -19,8 +58,18 @@ function checkEmail(email) {
     }
 }
 
-function checkFirstName() {}
-
-function checkLastName() {}
-
-function checkPassword() {}
+// checks if the user entered a password
+function checkPassword(password) {
+    var password = $('input[name=password]').val();
+    if(password == false){
+        var passError = document.querySelector('#passerror');
+        var ex4 = document.querySelector('#ex4');
+        var passInput = document.querySelector('#input4')
+        // unhide the error message
+        passError.style.visibility = "visible";
+        // unhide the exclamation point
+        ex4.style.display = "block";
+        // turn the border around the input field red
+        passInput.style.border = "2px solid #FF7979";
+    }
+}
