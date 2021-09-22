c2eb71d5ba4eec5810743e3c74e8f48b0daf2029
Heather Smith <halexsmith86@gmail.com>
2021-09-22 12:44:43 -0400

    gave all  inputs onblur functionality

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function submitForm(){
 // checks if user entered a first name
 function checkFirstName(firstName) {
     var firstName = $('input[name=first]').val();
+    var firstError = document.querySelector('#firsterror');
+    var ex1 = document.querySelector('#ex1');
+    var firstInput = document.querySelector('#input1')
     if(firstName == false){
-        var firstError = document.querySelector('#firsterror');
-        var ex1 = document.querySelector('#ex1');
-        var firstInput = document.querySelector('#input1')
         // unhide the error message
         firstError.style.visibility = "visible";
         // unhide the exclamation point
@@ -20,21 +20,37 @@ function checkFirstName(firstName) {
         // turn the border around the input field red
         firstInput.style.border = "2px solid #FF7979";
     }
+    if(firstName){
+        // hide the error message
+        firstError.style.visibility = "hidden";
+        // hide the exclamation point
+        ex1.style.display = "none";
+        // change the border back
+        firstInput.style.border = "1px solid #DEDEDE";
+    }
 }
 
 // checks if user entered a last name
 function checkLastName(lastName) {
     var lastName = $('input[name=last]').val();
+    var lastError = document.querySelector('#lasterror');
+    var ex2 = document.querySelector('#ex2');
+    var lastInput = document.querySelector('#input2')
     if(lastName == false){
-        var lastError = document.querySelector('#lasterror');
-        var ex2 = document.querySelector('#ex2');
-        var lastInput = document.querySelector('#input2')
         // unhide the error message
         lastError.style.visibility = "visible";
         // unhide the exclamation point
         ex2.style.display = "block";
         // turn the border around the input field red
         lastInput.style.border = "2px solid #FF7979";
+    }
+    if(lastName){
+        // hide the error message
+        lastError.style.visibility = "hidden";
+        // hide the exclamation point
+        ex2.style.display = "none";
+        // change the border back
+        lastInput.style.border = "1px solid #DEDEDE";
     }
 }
 
@@ -44,17 +60,25 @@ function checkEmail(email) {
     var email = $('input[name=email]').val();
     // regex
     var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailError = document.querySelector('#emailerror');
+    var ex3 = document.querySelector('#ex3');
+    var emailInput = document.querySelector('#input3');
     // if email field is blank or not an email address...
     if(!re.test(email)){
-        var emailError = document.querySelector('#emailerror');
-        var ex3 = document.querySelector('#ex3');
-        var emailInput = document.querySelector('#input3');
         // unhide the error message
         emailError.style.visibility = "visible";
         // unhide the exclamation point
         ex3.style.display = "block";
         // turn the border around the input field red
         emailInput.style.border = "2px solid #FF7979";
+    }
+    if(email){
+        // hide the error message
+        emailError.style.visibility = "hidden";
+        // hide the exclamation point
+        ex3.style.display = "none";
+        // change the border back
+        emailInput.style.border = "1px solid #DEDEDE";
     }
 }
 
@@ -77,7 +101,7 @@ function checkPassword(password) {
         passError.style.visibility = "hidden";
         // hide the exclamation point
         ex4.style.display = "none";
-        // turn the border around the input field red
+        // change the border back
         passInput.style.border = "1px solid #DEDEDE";
     }
 }
